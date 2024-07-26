b5809dae4b41fe26df3acd493dd656c01ee5f486
Bugra Kagan <Bugra Kagan@DESKTOP-MVI0BA2>
2024-07-27 02:18:32 +0300

    Proje dosyası ekle.

---
+++ ImageDetecth6/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace ImageDetecth6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 65);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "YOLOV 8 Nano Cl";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 245);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 65);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "YOLOV 8 Nano";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(14, 316);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(174, 65);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Haar-Cascade(F-E Only)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(13, 387);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(174, 65);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Keras-TL(Passive Now)";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(14, 463);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(174, 65);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Retry";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(837, 103);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(221, 420);
+            this.listBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(194, 103);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(637, 425);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(360, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(380, 48);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "- Image Detect V6 -";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1069, 564);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Image Detect V6";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ImageDetecth6/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageDetecth6
+{
+    public partial class Form1 : Form
+    {
+        private string outputImagePath = @"D:\cam6\output.jpg";  
+        public Form1()
+        {
+            InitializeComponent();
+            this.button6.Click += new System.EventHandler(this.button6_Click); 
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+                openFileDialog.Multiselect = true;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string fileName in openFileDialog.FileNames)
+                    {
+                        listBox1.Items.Add(fileName);
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string selectedImagePath = listBox1.SelectedItem.ToString();
+                RunPythonScript(@"D:\cam6\cam1.py", selectedImagePath);
+                DisplayOutputImage();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen listeden bir resim seçin.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string selectedImagePath = listBox1.SelectedItem.ToString();
+                RunPythonScript(@"D:\cam6\cam2.py", selectedImagePath);
+                DisplayOutputImage();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen listeden bir resim seçin.");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string selectedImagePath = listBox1.SelectedItem.ToString();
+                RunPythonScript(@"D:\cam6\haarcam2.py", selectedImagePath);
+                DisplayOutputImage();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen listeden bir resim seçin.");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string selectedImagePath = listBox1.SelectedItem.ToString();
+                RunPythonScript(@"D:\cam6\all.py", selectedImagePath);
+                DisplayOutputImage();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen listeden bir resim seçin.");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+
+            string[] filesToDelete =
+            {
+        @"D:\cam6\output.jpg", 
+    };
+
+            foreach (string filePath in filesToDelete)
+            {
+                if (File.Exists(filePath))
+                {
+                    try
+                    {
+                        
+                        GC.Collect();
+                        GC.WaitForPendingFinalizers();
+                        File.Delete(filePath);
+                        MessageBox.Show($"{Path.GetFileName(filePath)} başarıyla silindi.");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Dosya silinirken bir hata oluştu: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"{Path.GetFileName(filePath)} bulunamadı.");
+                }
+            }
+
+            
+            listBox1.Items.Clear();
+        }
+
+        private void RunPythonScript(string scriptPath, string inputImagePath)
+        {
+            string pythonExePath = @"C:\Users\Bugra Kagan\AppData\Local\Programs\Python\Python311\python.exe";
+            string outputImagePath = @"D:\cam6\output.jpg";
+
+            ProcessStartInfo start = new ProcessStartInfo
+            {
+                FileName = pythonExePath,
+                Arguments = $"\"{scriptPath}\" \"{inputImagePath}\" \"{outputImagePath}\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true
+            };
+
+            try
+            {
+                using (Process process = Process.Start(start))
+                {
+                    using (StreamReader outputReader = process.StandardOutput)
+                    {
+                        string result = outputReader.ReadToEnd();
+                        MessageBox.Show(result); 
+                    }
+                    using (StreamReader errorReader = process.StandardError)
+                    {
+                        string error = errorReader.ReadToEnd();
+                        if (!string.IsNullOrEmpty(error))
+                        {
+                            MessageBox.Show($"Error: {error}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+        private void DisplayOutputImage()
+        {
+            if (File.Exists(outputImagePath))
+            {
+                pictureBox1.Image = new Bitmap(outputImagePath);
+            }
+            else
+            {
+                MessageBox.Show("Görüntü dosyası bulunamadı.");
+            }
+        }
+    }
+}
